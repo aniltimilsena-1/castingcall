dd3dad6dc2ab181863c4adc64b15028483f8c2dd
aniltimilsena-1 <aniltimilsena53@gmail.com>
2026-03-13 17:15:03 +0900

    Remove all iOS/Android mobile app related files and dependencies

--- android/app/src/main/java/com/userhub/expansion/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.userhub.expansion;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.caastingcall.me',
-  appName: 'CaastingCall',
-  webDir: 'dist'
-};
-
-export default config;
